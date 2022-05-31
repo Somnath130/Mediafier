596a31db06448dbac32d84e9d4195e8443944149
Somnath130 <somnath.goswami@cginfinity.com>
2022-05-31 19:05:34 +0530

    Added validations

--- JS/login.js
+++ JS/login.js
@@ -18,3 +18,56 @@ Swal.fire({
 });
 } 
 
+//Email address validation
+
+var emailAddress = document.getElementById("logmail");
+var emailAddressValidation = function () {
+  emailAddressValue = emailAddress.value.trim();
+  validEmailAddress = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  emailAddressErr = document.getElementById("emailtag");
+  emailAddressErr.style.color = "red";
+  if (emailAddressValue == "") {
+    emailAddressErr.innerHTML = "Email Address is required";
+  } else if (!validEmailAddress.test(emailAddressValue)) {
+    emailAddressErr.innerHTML =
+      "Email Address must be in valid format with @ symbol";
+  } 
+  else {
+    emailAddressErr.innerHTML = "";
+    return true;
+  }
+};
+
+logmail.oninput = function () {
+  emailAddressValidation();
+};
+
+
+//password validation
+var passwordvalid = document.getElementById("loginpasswd");
+passwordvalid.setAttribute("type", "password");
+
+var passwordValidation = function () {
+  passwordValue = passwordvalid.value.trim();
+  validPassword =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  passwordErr = document.getElementById("pswdtag");
+
+  passwordErr.style.color = "red";
+
+  if (passwordValue == "") {
+    passwordErr.innerHTML = "Password is required";
+  } else if (!validPassword.test(passwordValue)) {
+    passwordErr.innerHTML =
+      "Password must have at least one Uppercase, lowercase, digit, special characters & 8 characters";
+  } else {
+    passwordErr.innerHTML = "";
+    return true;
+  }
+};
+
+passwordvalid.oninput = function () {
+  passwordValidation();
+ //  confirmPasswordValidation();
+};
